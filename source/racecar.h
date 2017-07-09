894a7fa22756ebf58328cf5a7e0c3cbfdb8c0d61
Tim Beaudet <timbeaudet@users.noreply.github.com>
2017-07-09 11:12:43 -0400

    Created racecar.h

---
+++ source/racecar.h
@@ -0,0 +1,6 @@
+///
+/// @file
+/// @details Provides includes for the different components of the racecar drivetrain.
+///
+/// <!-- Copyright (c) Tim Beaudet 2017 - All Rights Reserved -->
+///-----------------------------------------------------------------------------------------------------------------///
