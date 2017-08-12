00c6bfcee4a65d1eb7d33e7f968489882a72d0ed
Tim Beaudet <timbeaudet@users.noreply.github.com>
2017-08-12 18:38:44 -0400

    Adding more tests!

--- unit_test_source/basic_test.cpp
+++ unit_test_source/basic_test.cpp
@@ -34,16 +34,58 @@ public:
 	}
 };
 
+bool ConstructionTest(void);
+bool ConstantTorqueTest(void);
+
 bool Racecar::UnitTests::PerformBasicTests(void)
 {
-	RotatingMass rotatingMass(10.0f); //10kg-m.
-	RotatingMass rotatingMass2(10.0f, 1.0f);
+	if (false == ConstructionTest()) { log_test("Failed: Expected to pass the ConstructionTest().\n"); return false; }
+	if (false == ConstantTorqueTest()) { log_test("Failed: Expected to pass the ConstantTorqueTest().\n"); return false; }
 
-	if (fabsf(rotatingMass.GetInertia() - rotatingMass2.GetInertia() > kTestElipson))
+	log_test("The Racecar has successfully passed technical inspection. You may now go racing!");
+	return true;
+}
+
+
+bool ConstructionTest(void)
+{
+	RotatingMass rotatingMass(10.0f); //10kg-m.
+	if (fabsf(rotatingMass.GetInertia() - 10.0f) > kTestElipson)
 	{
 		log_test("Failed accessing expected moments of inertia.");
 		return false;
 	}
 
+	RotatingMass rotatingMass2(10.0f, 1.0f);
+	if (fabsf(rotatingMass.GetInertia() - rotatingMass2.GetInertia()) > kTestElipson)
+	{
+		log_test("Failed accessing expected moments of inertia.");
+		return false;
+	}
+
+	const float startingVelocity(rotatingMass.GetAngularVelocity());
+	if (fabsf(startingVelocity) > kTestElipson)
+	{
+		log_test("Failed: Expected rotating body to have no angular velocity upon construction. Found: %f\n", startingVelocity);
+		return false;
+	}
+
+	return true;
+}
+
+bool ConstantTorqueTest(void)
+{
+	RotatingMass rotatingMass(10.0f); //10kg-m.
+
+	//Simulate 1 second of applying a 10Nm torque to the rotating body.
+	for (int timer(0); timer < 1000; timer += 10)
+	{
+		rotatingMass.ApplyDownstreamTorque(10.0f, rotatingMass);
+		rotatingMass.Simulate(); //Simulates 10ms of action.
+	}
+
+	const float finalVelocity(rotatingMass.GetAngularVelocity());
+	log_test("Final velocity: %f\n", finalVelocity);
+	
 	return false;
 }
