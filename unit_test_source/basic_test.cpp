d324c5b38afc005fbe7f25f739ec2678634283a5
Tim Beaudet <timbeaudet@users.noreply.github.com>
2017-08-12 18:07:14 -0400

    The very first unit test for the racecar suite.

--- unit_test_source/basic_test.cpp
+++ unit_test_source/basic_test.cpp
@@ -1,1 +1,49 @@
+///
+/// @file
+/// @details Defines the functions that perform the basic tests for the racecar drivetrain.
+///
+/// <!-- Copyright (c) Tim Beaudet 2017 - All Rights Reserved -->
+///-----------------------------------------------------------------------------------------------------------------///
 
+#include "basic_test.h"
+
+#include "../racecar/racecar_wheel.h"
+
+#include <cstdio>
+
+#define log_test(message, ...)   printf(message, ##__VA_ARGS__)
+static const float kTestElipson(0.0001f);
+
+class RotatingMass : public Racecar::RotatingBody
+{
+public:
+	explicit RotatingMass(const float momentOfInertia) :
+		RotatingBody()
+	{
+		SetInertia(momentOfInertia);
+	}
+
+	RotatingMass(const float massInKilograms, const float radiusInMeters) :
+		RotatingBody()
+	{
+		SetInertia(Racecar::ComputeInertiaMetric(massInKilograms, radiusInMeters));
+	}
+
+	virtual ~RotatingMass(void)
+	{
+	}
+};
+
+bool Racecar::UnitTests::PerformBasicTests(void)
+{
+	RotatingMass rotatingMass(10.0f); //10kg-m.
+	RotatingMass rotatingMass2(10.0f, 1.0f);
+
+	if (fabsf(rotatingMass.GetInertia() - rotatingMass2.GetInertia() > kTestElipson))
+	{
+		log_test("Failed accessing expected moments of inertia.");
+		return false;
+	}
+
+	return false;
+}
---
+++ unit_test_source/basic_test.h
@@ -0,0 +1,21 @@
+///
+/// @file
+/// @details Defines the functions that perform the basic tests for the racecar drivetrain.
+///
+/// <!-- Copyright (c) Tim Beaudet 2017 - All Rights Reserved -->
+///-----------------------------------------------------------------------------------------------------------------///
+
+#ifndef _Racecar_BasicTest_h_
+#define _Racecar_BasicTest_h_
+
+namespace Racecar
+{
+	namespace UnitTests
+	{
+
+		bool PerformBasicTests(void);
+
+	};
+};
+
+#endif /* _Racecar_BasicTest_h_ */
