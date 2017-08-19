3e1b8cf0d155b4a80207321100bee7e47f0a3894
Tim Beaudet <timbeaudet@users.noreply.github.com>
2017-08-19 03:35:59 -0400

    Adding the car body to represent the physical properties of the car.

---
+++ source/racecar_body.cpp
@@ -0,0 +1,42 @@
+///
+/// @file
+/// @details This is a simple simulation of wheel with a braking force provided by brakes on controller.
+///
+/// <!-- Copyright (c) Tim Beaudet 2017 - All Rights Reserved -->
+///-----------------------------------------------------------------------------------------------------------------///
+
+#include "racecar_body.h"
+
+//-------------------------------------------------------------------------------------------------------------------//
+
+Racecar::RacecarBody::RacecarBody(const Real mass) :
+	mMass(mass),
+	mLinearAcceleration(0.0),
+	mLinearVelocity(0.0)
+{
+}
+
+//-------------------------------------------------------------------------------------------------------------------//
+
+Racecar::RacecarBody::~RacecarBody(void)
+{
+}
+
+//-------------------------------------------------------------------------------------------------------------------//
+
+void Racecar::RacecarBody::Simulate(const Racecar::RacecarControllerInterface& racecarController, const Real fixedTime)
+{
+	((void)racecarController);
+
+	mLinearVelocity += mLinearAcceleration * fixedTime;
+	mLinearAcceleration = 0.0;
+}
+
+//-------------------------------------------------------------------------------------------------------------------//
+
+void Racecar::RacecarBody::ApplyForce(const Real& forceInNewtons)
+{
+	mLinearAcceleration += forceInNewtons / mMass;
+}
+
+//-------------------------------------------------------------------------------------------------------------------//
---
+++ source/racecar_body.h
@@ -0,0 +1,42 @@
+///
+/// @file
+/// @details This is a simple simulation of wheel with a braking force provided by brakes on controller.
+///
+/// <!-- Copyright (c) Tim Beaudet 2017 - All Rights Reserved -->
+///-----------------------------------------------------------------------------------------------------------------///
+
+#ifndef _Racecar_Body_h_
+#define _Racecar_Body_h_
+
+#include "racecar.h"
+
+namespace Racecar
+{
+	class RacecarControllerInterface;
+
+	class RacecarBody
+	{
+	public:
+		explicit RacecarBody(const Real mass); //kg
+		virtual ~RacecarBody(void);
+
+		///
+		///
+		///
+		void Simulate(const Racecar::RacecarControllerInterface& racecarController, const Real fixedTime = Racecar::kFixedTimeStep);
+
+		///
+		/// @details Applies a force to the body of the car, which will effect the accelerations for the next Simulate step.
+		///
+		void ApplyForce(const Real& forceInNewtons);
+
+	protected:
+
+	private:
+		Real mMass;
+		Real mLinearAcceleration;
+		Real mLinearVelocity;
+	};
+};	/* namespace Racecar */
+
+#endif /* _Racecar_Body_h_ */
