6cc34c56de19b9c69934c1f00ee066e99880d6a6
Tim Beaudet <timbeaudet@users.noreply.github.com>
2017-10-14 15:00:25 -0400

    Adding tests for the clutch component of the racecar drive-train.

---
+++ unit_test_source/clutch_test.cpp
@@ -0,0 +1,73 @@
+///
+/// @file
+/// @details A handful of test functions for testing the clutch component of the drive-train.
+///
+/// <!-- Copyright (c) Tim Beaudet 2017 - All Rights Reserved -->
+///-----------------------------------------------------------------------------------------------------------------///
+
+#include "clutch_test.h"
+#include "test_kit.h"
+
+#include "../player_racecar_controller.h"
+#include "../racecar/racecar.h"
+#include "../racecar/racecar_clutch.h"
+#include "../racecar/racecar_wheel.h"
+
+//--------------------------------------------------------------------------------------------------------------------//
+
+bool Racecar::UnitTests::ClutchInputTest(void)
+{
+	Racecar::ProgrammaticController racecarController;
+
+	const Real initialWheelAngularVelocity(40.0); //rad/s
+	Racecar::Wheel inputWheel(8.0, 0.25);
+	inputWheel.SetAngularVelocity(initialWheelAngularVelocity);
+
+	Racecar::Clutch clutch(inputWheel.GetInertia()); //kg*m^2
+	clutch.SetAngularVelocity(-initialWheelAngularVelocity);
+
+	clutch.SetInputSource(&inputWheel);
+	inputWheel.AddOutputSource(&clutch);
+
+	//Simulate 2 seconds with the clutch disengaged, nothing should change.
+	racecarController.SetClutchPosition(1.0f); //Disengage the clutch!
+	for (int timer(10); timer < 2000; timer += 10)
+	{
+		clutch.Simulate(racecarController, 0.01);
+		inputWheel.Simulate(racecarController, 0.01);
+	}
+
+	{	//Everything should be in the same state it started in initially.
+		if (fabs(inputWheel.GetAngularVelocity() - initialWheelAngularVelocity) > UnitTests::kTestElipson)
+		{
+			return false;
+		}
+		if (fabs(clutch.GetAngularVelocity() + initialWheelAngularVelocity) > UnitTests::kTestElipson)
+		{
+			return false;
+		}
+	}
+
+	//Simulate 2 seconds with clutch engaged.
+	racecarController.SetClutchPosition(0.0f); //Engage the clutch!
+	for (int timer(10); timer < 20000; timer += 10)
+	{
+		clutch.Simulate(racecarController, 0.01);
+		inputWheel.Simulate(racecarController, 0.01);
+	}
+
+	{	//Everything should be in the same state it started in initially.
+		if (fabs(inputWheel.GetAngularVelocity()) > UnitTests::kTestElipson)
+		{
+			return false;
+		}
+		if (fabs(clutch.GetAngularVelocity()) > UnitTests::kTestElipson)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+//--------------------------------------------------------------------------------------------------------------------//
---
+++ unit_test_source/clutch_test.h
@@ -0,0 +1,19 @@
+///
+/// @file
+/// @details A handful of test functions for testing the clutch component of the drive-train.
+///
+/// <!-- Copyright (c) Tim Beaudet 2017 - All Rights Reserved -->
+///-----------------------------------------------------------------------------------------------------------------///
+
+#ifndef _Racecar_ClutchTest_h_
+#define _Racecar_ClutchTest_h_
+
+namespace Racecar
+{
+	namespace UnitTests
+	{
+		bool ClutchInputTest(void);
+	};
+};
+
+#endif /* _Racecar_ClutchTest_h_ */
