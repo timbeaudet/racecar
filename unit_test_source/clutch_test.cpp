78316f2e6c96ef91dea82a8ad73535dcd720d59a
Tim Beaudet <timbeaudet@yahoo.com>
2017-10-15 11:37:04 -0400

    Added another test for the clutch to ensure it works.

--- unit_test_source/clutch_test.cpp
+++ unit_test_source/clutch_test.cpp
@@ -14,57 +14,83 @@
 #include "../racecar/racecar_clutch.h"
 #include "../racecar/racecar_wheel.h"
 
+#include <array>
+
 //--------------------------------------------------------------------------------------------------------------------//
+
+struct ClutchTestBlob
+{
+	Racecar::Real staticFriction;
+	Racecar::Real kineticFriction;
+	Racecar::Real maximumNormalForce;
+	Racecar::Real inputInertia;
+	Racecar::Real inputAngularVelocity;
+	Racecar::Real outputInertia;
+	Racecar::Real outputAngularVelocity;
+	Racecar::Real mExpectedInputAngularVelocity[2]; //After a single step, after all steps.
+	Racecar::Real mExpectedOutputAngularVelocity[2]; //After a single step, after all steps.
+	int mTestTime;
+};
 
 bool Racecar::UnitTests::ClutchInputTest(void)
 {
-	Racecar::ProgrammaticController racecarController;
+	std::array<ClutchTestBlob, 2> tests{
+		ClutchTestBlob{ 0.6, 0.4, 1000,     10.0, 40.0,     10.0, -40.0,     { 0.0, 0.0 }, { 0.0, 0.0 }, 1000 },
+		ClutchTestBlob{ 0.6, 0.4, 1000,     10.0, 40.0,     10.0,   0.0,     { 0.0, 20.0 }, { 0.0, 20.0 }, 1000 },
+	};
 
-	const Real initialWheelAngularVelocity(40.0); //rad/s
-	Racecar::Wheel inputWheel(8.0, 0.25);
-	inputWheel.SetAngularVelocity(initialWheelAngularVelocity);
-
-	Racecar::Clutch clutch(inputWheel.GetInertia()); //kg*m^2
-	clutch.SetAngularVelocity(-initialWheelAngularVelocity);
-
-	clutch.SetInputSource(&inputWheel);
-	inputWheel.AddOutputSource(&clutch);
-
-	//Simulate 2 seconds with the clutch disengaged, nothing should change.
-	racecarController.SetClutchPosition(1.0f); //Disengage the clutch!
-	for (int timer(10); timer < 2000; timer += 10)
+	for (const ClutchTestBlob& test : tests)
 	{
-		clutch.Simulate(racecarController, 0.01);
-		inputWheel.Simulate(racecarController, 0.01);
-	}
+		Racecar::ProgrammaticController racecarController;
 
-	{	//Everything should be in the same state it started in initially.
-		if (fabs(inputWheel.GetAngularVelocity() - initialWheelAngularVelocity) > UnitTests::kTestElipson)
-		{
-			return false;
-		}
-		if (fabs(clutch.GetAngularVelocity() + initialWheelAngularVelocity) > UnitTests::kTestElipson)
-		{
-			return false;
-		}
-	}
+		//Racecar::Wheel inputWheel(8.0, 0.25);
+		Racecar::RotatingBody inputBody(test.inputInertia);
+		inputBody.SetAngularVelocity(test.inputAngularVelocity);
 
-	//Simulate 2 seconds with clutch engaged.
-	racecarController.SetClutchPosition(0.0f); //Engage the clutch!
-	for (int timer(10); timer < 20000; timer += 10)
-	{
-		clutch.Simulate(racecarController, 0.01);
-		inputWheel.Simulate(racecarController, 0.01);
-	}
+		Racecar::Clutch clutch(test.outputInertia, 1000, 0.6, 0.4); //kg*m^2
+		clutch.SetAngularVelocity(test.outputAngularVelocity);
 
-	{	//Everything should be in the same state it started in initially.
-		if (fabs(inputWheel.GetAngularVelocity()) > UnitTests::kTestElipson)
+		clutch.SetInputSource(&inputBody);
+		inputBody.AddOutputSource(&clutch);
+
+		//Simulate 2 seconds with the clutch disengaged, nothing should change.
+		racecarController.SetClutchPosition(1.0f); //Disengage the clutch!
+		for (int timer(10); timer < 2000; timer += 10)
 		{
-			return false;
+			clutch.Simulate(racecarController, 0.01);
+			inputBody.Simulate(0.01);
 		}
-		if (fabs(clutch.GetAngularVelocity()) > UnitTests::kTestElipson)
+
+		{	//Everything should be in the same state it started in initially.
+			if (fabs(inputBody.GetAngularVelocity() - test.inputAngularVelocity) > UnitTests::kTestElipson)
+			{
+				return false;
+			}
+			if (fabs(clutch.GetAngularVelocity() - test.outputAngularVelocity) > UnitTests::kTestElipson)
+			{
+				return false;
+			}
+		}
+
+		//TODO: Compute and test a single time-step.
+
+		//Simulate 2 seconds with clutch engaged.
+		racecarController.SetClutchPosition(0.0f); //Engage the clutch!
+		for (int timer(10); timer < 20000; timer += 10)
 		{
-			return false;
+			clutch.Simulate(racecarController, 0.01);
+			inputBody.Simulate(0.01);
+		}
+
+		{	//Everything should be in the same state it started in initially.
+			if (fabs(inputBody.GetAngularVelocity() - test.mExpectedInputAngularVelocity[1]) > UnitTests::kTestElipson)
+			{
+				return false;
+			}
+			if (fabs(clutch.GetAngularVelocity() - test.mExpectedOutputAngularVelocity[1]) > UnitTests::kTestElipson)
+			{
+				return false;
+			}
 		}
 	}
 
