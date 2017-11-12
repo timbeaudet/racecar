13fa0f10d76fd0a1bcdabaca1164358763115e29
Tim Beaudet <timbeaudet@yahoo.com>
2017-11-12 15:31:26 -0500

    Added a kit header to include all others from a single include.

---
+++ source/racecar_kit.h
@@ -0,0 +1,20 @@
+///
+/// @file
+/// @details Includes all of the Racecar Components as a single header include for convenience.
+///
+/// <!-- This file is made available under the terms of the MIT license(see LICENSE.md) -->
+/// <!-- Copyright (c) 2017 Contributers: Tim Beaudet, -->
+///-----------------------------------------------------------------------------------------------------------------///
+
+#ifndef _Racecar_RacecarKit_h_
+#define _Racecar_RacecarKit_h_
+
+#include "racecar.h"
+#include "racecar_engine.h"
+#include "racecar_clutch.h"
+#include "racecar_transmission.h"
+#include "racecar_locked_differential.h"
+#include "racecar_wheel.h"
+#include "racecar_body.h"
+
+#endif /* _Racecar_RacecarKit_h_ */
